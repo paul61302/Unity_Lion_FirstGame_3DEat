aeca4aeaa1334d45a392f95f65d055529f1a3a2a
paul61302 <paul61302@gmail.com>
2020-06-02 13:04:24 +0800

    123

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -28,7 +30,7 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// 遊戲時間
     /// </summary>
-    private float gameTime = 30.5f;
+    private float gameTime = 60f;
     #endregion
 
     #region 方法
@@ -54,29 +56,105 @@ public class GameManager : MonoBehaviour
         return total;
     }
 
+    /// <summary>
+    /// 時間倒數
+    /// </summary>
     private void CountTime()
     {
+        // 如果取得所有雞腿，就跳出
+        if (countprop == countTotal) return;
+
         // 遊戲時間 遞減 一禎的時間
         gameTime -= Time.deltaTime;
+
+        // 遊戲時間 = 數學.夾住(遊戲時間,最小值,最大值)
+        gameTime = Mathf.Clamp(gameTime, 0, 100);
+
         // 更新倒數時間介面 ToString("f小數點位數")
         textTime.text = "倒數時間：" + gameTime.ToString("f2");
+
+        Lose();
+    }
+
+    /// <summary>
+    /// 取得道具：HotDog - 更新數量與介面，葡萄酒 - 扣兩秒並更新介面
+    /// </summary>
+    /// <param name="prop">道具名稱</param>
+    public void GetProp(string prop)
+    {
+        if (prop == "HotDog")
+        {
+            countprop++;
+            textCount.text = "道具數量：" + countprop + " / " + countTotal;
+
+            Win();          // 呼叫勝利方法
+        }
+        else if (prop == "葡萄酒")
+        {
+            gameTime -= 2;
+            textTime.text = "倒數時間：" + gameTime.ToString("f2");
+        }
+    }
+
+    /// <summary>
+    /// 勝利：吃光所有HotDog
+    /// </summary>
+    private void Win()
+    {
+        if (countprop == countTotal)           // 如果 HotDog 數量 = HotDog 總數
+        {
+            final.alpha = 1;                   // 顯示結束畫面、啟動互動、啟動遮擋
+            final.interactable = true;
+            final.blocksRaycasts = true;
+            textTitle.text = "恭喜過關～";       // 更新結束畫面標題
+            FindObjectOfType<Player>().enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// 失敗：時間為零
+    /// </summary>
+    private void Lose()
+    {
+        if (gameTime == 0)
+        {
+            final.alpha = 1;
+            final.interactable = true;
+            final.blocksRaycasts = true;
+            textTitle.text = "恭喜沒有過關～";
+            FindObjectOfType<Player>().enabled = false;     // 取得玩家.啟動 = false
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void Quit()
+    {
+        Application.Quit(); // 應用程式.離開()
     }
     #endregion
 
     #region 事件
     private void Start()
     {
-        countTotal = CreateProp(porps[0], 20);      // 道具總數 = 生成道具(道具一號,指定數量)
+        countTotal = CreateProp(porps[0], 10);      // 道具總數 = 生成道具(道具一號,指定數量)
 
         textCount.text = "道具數量：0 / " + countTotal;
 
         CreateProp(porps[1], 10);
     }
 
+   
+
     private void Update()
     {
         CountTime();
     }
+
+    
     #endregion
 
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -27,13 +27,20 @@ public class Player : MonoBehaviour
     /// </summary>
     private Vector3 angle;
     
-    private Animator ani;
-    private Rigidbody rig;
+    private Animator ani;           // 動畫
+    private Rigidbody rig;          // 剛體
+    private AudioSource aud;        // 喇叭
+    private GameManager gm;         // 遊戲管理器
 
     /// <summary>
     ///  跳躍力道：從 0 慢慢增加
     /// </summary>
     private float jump;
+
+    [Header("熱狗堡音效")]
+    public AudioClip SoundHotdog;
+    [Header("葡萄酒音效")]
+    public AudioClip SoundWine;
     #endregion
 
     #region 方法
@@ -115,9 +122,23 @@ public class Player : MonoBehaviour
     /// <summary>
     /// 碰到道具：碰到帶有標籤[HotDog]的物件
     /// </summary>
-    private void HitProp()
+    private void HitProp(GameObject prop)
     {
+        //Destroy()
+        if (prop.tag == "HotDog")
+        {
+            aud.Stop();
+            aud.PlayOneShot(SoundHotdog, 2);    // 喇叭.撥放一次音效(音效片段,音量)
+            Destroy(prop);                      // 刪除(物件)
+        }
+        else if (prop.tag == "葡萄酒")
+        {
+            aud.Stop();
+            aud.PlayOneShot(SoundWine, 2);
+            Destroy(prop);
+        }
 
+        gm.GetProp(prop.tag);               // 告知 GM 取得道具(將道具標籤船過去)
     }
     #endregion
 
@@ -128,6 +149,10 @@ public class Player : MonoBehaviour
         // 剛體 = 取得元件<剛體>();
         rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
+        // FindObjectOfType 僅限於場景上只有一個類別存在時使用
+        // 例如：場景上只有一個 GameManager 類別時可以使用他來取得
+        gm = FindObjectOfType<GameManager>();
     }
 
     // 固定更新頻率：1 秒 50 禎，使用物理(含Rigidbody)必須在此事件內(官網建議)
@@ -140,6 +165,41 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Jump();
+    }
+
+    // 碰撞事件：當物件碰撞開始時執行一次 (沒有勾選 Is Trigger)
+    // collision 碰撞物件的碰撞資訊
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    // 碰撞事件：當物件碰撞開始時執行一次 (沒有勾選 Is Trigger)
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+    // 碰撞事件：當物件碰撞開始時持續執行 (沒有勾選 Is Trigger) 60 FPS
+    private void OnCollisionStay(Collision collision)
+    {
+
+    }
+
+    /*----------*/
+    // 觸發事件：當物件碰撞開始時執行一次 (有勾選 Is Trigger)
+    private void OnTriggerEnter(Collider other)
+    {
+        // 碰到道具(碰撞資訊.遊戲物件)
+        HitProp(other.gameObject);
+    }
+    // 觸發事件：當物件碰撞開始時執行一次 (有勾選 Is Trigger)
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+    // 觸發事件：當物件碰撞開始時持續執行 (有勾選 Is Trigger) 60 FPS
+    private void OnTriggerStay(Collider other)
+    {
+
     }
     #endregion
 
